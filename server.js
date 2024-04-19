668c074190ca521384211fa17fbd7589b601c395
shubam rajput <113655062+SHUBHAMRAJPUT9665@users.noreply.github.com>
2024-04-19 19:20:12 +0530

    se

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 const express = require('express')
 const app = express();
-require('dotenv').config();
-
-const boydParser = require('body-parser');
-
-app.use(boydParser.json()); // req body
 
 const PORT = process.env.PORT || 3000
 
 
-app.listen(PORT , () =>{
-    console.log(`server is runing on port ${PORT}`)
+
+
+
+app.listen(PORT,()=>{
+    console.log(`server is running on port ${PORT}`)
 })
