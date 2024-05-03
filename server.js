9acd107458ef30cafa503f51af608362378effe9
shubam rajput <113655062+SHUBHAMRAJPUT9665@users.noreply.github.com>
2024-05-03 19:38:26 +0530

    config

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-const express = require('express')
+const express = require("express")
+
 const app = express();
 
-const PORT = process.env.PORT || 3000
+PORT = process.env.PORT || 8000
 
-
-
-
+app.get('/',(req,res)=>{
+    res.send("hii welcome");
+})
 
 app.listen(PORT,()=>{
-    console.log(`server is running on port ${PORT}`)
+    console.log(`server is runing on PORT ${PORT}`)
 })
