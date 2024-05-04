331696dbb88f6cb70bab4aae30cb449363b464c9
shubam rajput <113655062+SHUBHAMRAJPUT9665@users.noreply.github.com>
2024-05-04 09:00:09 +0530

    bodyparser

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express")
-
 const app = express();
+require('dotenv').config();
 
+
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 PORT = process.env.PORT || 8000
 
 app.get('/',(req,res)=>{
