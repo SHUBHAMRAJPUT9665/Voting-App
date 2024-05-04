6a1821946c3d8067e4ce7af8c07c1c5e60b345ed
shubam rajput <113655062+SHUBHAMRAJPUT9665@users.noreply.github.com>
2024-05-05 03:36:58 +0530

    login route

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,34 +1,56 @@
-const express = require("express")
+const express = require("express");
 const router = express.Router();
 
-const User = require('./../models/user')
-const {jwtAuthMiddleware , generateToken} = require('./../jwt')
+const User = require("./../models/user");
+const { jwtAuthMiddleware, generateToken } = require("./../jwt");
 
+router.post("/signup", async (req, res) => {
+  try {
+    const data = req.body;
 
+    const newUser = new User(data);
 
-router.post('/signup' , async (req,res) =>{
-    try {
-        const data = req.body
+    const response = await newUser.save();
 
-        const newUser = new User(data)
+    console.log("data saved");
 
-        const response = await newUser.save()
+    const payload = {
+      id: res.id,
+      username: response.username,
+    };
+    console.log(JSON.stringify(payload));
+    const token = generateToken(payload);
 
-        console.log("data saved")
+    console.log("Token is: ", token);
 
-        const payload = {
-            id:res.id , 
-            username:response.username
-        }
-        console.log(JSON.stringify(payload))
-        const token = generateToken(payload)
+    res.status(200).json({ response: response, token: token });
+  } catch (error) {
+    console.log(error);
 
-        console.log("Token is: ",token)
+    res.status(500).json({ error: "Internal server Error" });
+  }
+});
 
-        res.status(200).json({response: response , token:token})
-    } catch (error) {
-        console.log(error)
+router.post("/login", async (req, res) => {
+  try {
+    const { aadharCardNumber, password } = req.body;
 
-        res.status(500).json({error:"Internal server Error"});
+    const user = await User.findOne({ aadharCardNumber: aadharCardNumber });
+
+    if (!user || !(await user.comparePassword(password))) {
+      return res.status(401).json({ error: "Invalid username or password" });
     }
-})
+
+    const payload = {
+      id: user.id,
+    };
+
+    const token = generateToken(payload);
+
+    res.json({ token });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({ error: "invalid Server Error" });
+  }
+});
