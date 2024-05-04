9d4946dfb91795aaacb307d1795c4beaf060798e
shubam rajput <113655062+SHUBHAMRAJPUT9665@users.noreply.github.com>
2024-05-05 03:18:06 +0530

    signup route

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -2,3 +2,31 @@ const express = require("express")
 const router = express.Router();
 
 const User = require('./../models/user')
+const {jwtAuthMiddleware , generateToken} = require('./../jwt')
+
+
+
+router.post('/signup' , async (req,res) =>{
+    try {
+        const data = req.body
+
+        const newUser = new User(data)
+
+        const response = await newUser.save()
+
+        console.log("data saved")
+
+        const payload = {
+            id:res.id , 
+            username:response.username
+        }
+        console.log(JSON.stringify(payload))
+        const token = generateToken(payload)
+
+        console.log("Token is: ",token)
+
+        res.status(200).json({response: response , token:token})
+    } catch (error) {
+        
+    }
+})
